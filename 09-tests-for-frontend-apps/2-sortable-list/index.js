06f97c07faf78595a302791bcbeb3021e8d592f7
gluknik <gluknik@gmail.com>
2021-04-14 21:40:40 +0300

    Adds solution for 'Sortable List' task (without 2 test)

--- 09-tests-for-frontend-apps/2-sortable-list/index.js
+++ 09-tests-for-frontend-apps/2-sortable-list/index.js
@@ -1,3 +1,128 @@
 export default class SortableList {
+  onClick = (event) => {
+    if (event.target.dataset.deleteHandle !== undefined) {
+      this.deleteHandler(event.target.parentNode);
+    }
+  }
 
+  onMouseDown = (event) => {
+    if (event.target.dataset.grabHandle !== undefined) {
+      this.dragHandler(event, event.target.parentNode);
+    }
+  }
+
+  constructor(data) {
+    this.data = data;
+    this.render();
+    this.initEventListeners();
+  }
+
+  render() {
+    const element = document.createElement('ul');
+    element.classList.add('sortable-list');
+
+    element.innerHTML = this.data.items.map(item => {
+      item.classList.add('sortable-list__item');
+      return item.outerHTML;
+    }).join('');
+
+    this.element = element;
+  }
+
+  dragHandler(event, item) {
+    const itemHeight = item.getBoundingClientRect().height;
+    const itemWidth = item.getBoundingClientRect().width;
+    const draggableElement = item.cloneNode(true);
+    draggableElement.classList.add('sortable-list__item_dragging');
+    draggableElement.style.width = itemWidth + 'px';
+    item.classList.remove('sortable-list__item');
+    item.classList.add('sortable-list__placeholder');
+    item.innerHTML = '';
+    item.style.height = itemHeight + 'px';
+
+    let shiftX = event.clientX - item.getBoundingClientRect().left;
+    let shiftY = event.clientY - item.getBoundingClientRect().top;
+
+    this.element.append(draggableElement);
+
+    moveAt(event.pageX, event.pageY);
+    
+    function moveAt(pageX, pageY) {
+      draggableElement.style.left = pageX - shiftX + 'px';
+      draggableElement.style.top = pageY - shiftY + 'px';
+    }
+
+    let currentDroppable = item;
+    let wasMouseMove = false;
+
+    function onMouseMove(event) {
+      wasMouseMove = true;
+      moveAt(event.pageX, event.pageY);
+
+      draggableElement.style.display = 'none';
+      let elemBelow = document.elementFromPoint(event.clientX, event.clientY);
+      draggableElement.style.display = 'flex';
+
+      if (!elemBelow) return;
+
+      let droppableBelow = elemBelow.closest('.sortable-list__placeholder');
+      if (currentDroppable != droppableBelow) {
+        if (currentDroppable) {
+          const prevElem = currentDroppable.previousElementSibling;
+          const nextElem = currentDroppable.nextElementSibling;
+
+          if (prevElem) {
+            const middlePrevElem = prevElem.getBoundingClientRect().top + prevElem.getBoundingClientRect().height / 2;
+
+            if (event.clientY < middlePrevElem) {
+              return prevElem.before(currentDroppable);
+            }
+          }
+
+          if (nextElem) {
+            const middleNextElem = nextElem.getBoundingClientRect().top + nextElem.getBoundingClientRect().height / 2;
+
+            if (event.clientY > middleNextElem) {
+              return nextElem.after(currentDroppable);
+            }
+          }
+        }
+
+        if (droppableBelow) {
+          currentDroppable = droppableBelow;
+        }
+      }
+    }
+
+    document.addEventListener('mousemove', onMouseMove);
+
+
+    draggableElement.onmouseup = function() {
+      document.removeEventListener('mousemove', onMouseMove);
+      draggableElement.onmouseup = null;
+      currentDroppable.parentNode.replaceChild(draggableElement, currentDroppable);
+      draggableElement.classList.remove('sortable-list__item_dragging');
+      draggableElement.style.left = 'auto';
+      draggableElement.style.top = 'auto';
+    };
+    
+  }
+
+  deleteHandler(item) {
+    item.remove();
+  }
+
+  initEventListeners () {
+    this.element.addEventListener('click', this.onClick);
+    this.element.addEventListener('mousedown', this.onMouseDown);
+  }
+
+  remove () {
+    this.element.remove();
+  }
+
+  destroy() {
+    this.remove();
+    // NOTE: удаляем обработчики событий, если они есть
+  }
 }
